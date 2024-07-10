ab7a3c4e436786ce782ad86608f4ce95f53b0734
DinelyWellehewage <dinelywh78@gmail.com>
2024-07-10 22:14:44 +0200

    building color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,12 @@ function App() {
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area="nav" bg="coral">
+      <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg" />
-      <GridItem area="aside" bg="gold">
-        Aside
-      </GridItem>
-      <GridItem area="main" bg="doggelblue">
-        Main
-      </GridItem>
+      <GridItem area="aside">Aside</GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
